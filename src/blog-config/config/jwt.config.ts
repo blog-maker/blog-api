d78f309a97538fd6a750ceb3b0c1810b2c3cad36
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2020-04-26 11:13:22 -0300

    adding mongoose support.

--- src/blog-config/config/jwt.config.ts
+++ src/blog-config/config/jwt.config.ts
@@ -1,6 +1,6 @@
-import { registerAs } from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
   secret: process.env.JWT_SECRET,
-  expiresIn: process.env.JWT_EXPIRES_IN
-}))
+  expiresIn: process.env.JWT_EXPIRES_IN,
+}));
